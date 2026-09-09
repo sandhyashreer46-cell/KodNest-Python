13b8b59cc0faf7a885f2a40018891415476c0475
sandhyashreer46 <sandhyashreer46@gmail.com>
2026-09-09 15:51:43 +0530

    Day7-fundamentals

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,16 @@
+print("This is my first program")
+#Assignment: Basic print() in Python
+
+#Task: Write a Python program that prints the following information:
+
+print("your name: sandhyashree")
+print("your age: 21")
+print("your city: shivamogga")
+print("Your favorite programming language: python")
+print("The one week  of python journey went really well.I had fun learning python.Hopping for upcoming session")
+#Print the following pattern using only print() statements
+print("*")
+print("**")
+print("***")
+print("****")
+print("******")
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/pseudo.py
@@ -0,0 +1,56 @@
+# To print Hello world
+#start
+#  print "Hello World"
+#stop
+print ("Hello world \nThank you for learning python")
+print("Hello world",end=" - ")
+print("Thank you for learning python")
+name = ("sandhyashree")
+print (name)
+# To find the number  is pos,neg or zero
+"""START
+INPUT n
+IF n > 0 
+PRINT "positive"
+ELIF n < 0
+ PRINT "negative"
+ELSE
+ PRINT "zero"
+END"""
+n = 0
+if n > 0:
+    print("positive")
+elif n < 0:
+    print("negative")
+else:
+    print("zero") 
+# To find the largest among 3 numbers a,b,c
+"""START
+INPUT a
+INPUT b
+INPUT c
+IF a >= b AND a >= c
+    PRINT "a is largest"
+ELSE IF b >= a  AND b >= c
+    PRINT "b is largest"
+ELSE
+    PRINT "c is largest"
+END"""
+a = 10; b = 2; c = 0
+if a >= b and a >= c:
+    print("a is largest")
+elif b >= a and b >= c:
+    print("b is largest")
+else:
+    print("c is largest")
+# To check whether the number is even or odd
+"""START
+INPUT n
+IF n % 2 == 0
+ PRINT "even"
+ELSE
+ PRINT "odd"
+END"""
+
+
+
